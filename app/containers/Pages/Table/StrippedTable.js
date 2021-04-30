f57da995e1ace0af658dd4017cbc27364b02fd6a
harrypotter2021 <newalexey2020@gmail.com>
2021-04-30 17:48:30 +0900

    activity list page is fixed

--- app/containers/Pages/Table/StrippedTable.js
+++ app/containers/Pages/Table/StrippedTable.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-
+import { Field, reduxForm } from 'redux-form/immutable';
 import Chip from '@material-ui/core/Chip';
 import MUIDataTable from 'mui-datatables';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -19,8 +19,15 @@ import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import FormLabel from '@material-ui/core/FormLabel';
-
-import {getCategories, createActivity} from '../client.js';
+import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import {
+  TextFieldRedux,
+  SelectRedux,
+  CheckboxRedux,
+  SwitchRedux
+} from 'enl-components/Forms/ReduxFormMUI';
+import {cancelActivity, getActivities} from '../client.js';
 
 const styles = theme => ({
   table: {
@@ -81,7 +88,7 @@ function AdvFilter(props) {
             <InputLabel htmlFor="age-simple">More Options</InputLabel>
               <Select
                 // value={1}
-                // onChange={handleChange}
+                onChange={cancelOpen}
                 inputProps={{
                   name: 'Id'
                 }}
@@ -112,20 +119,74 @@ function AdvFilter(props) {
     responsive: 'stacked',
     print: true,
     rowsPerPage: 10,
-    page: 0
+    page: 0,
+    viewColumns: true,
+    selectableRows: 'none',
+    onRowClick: (rowData, rowState) => {
+      setRowindexState({
+        ...rowindexState, dataIndex:rowState.dataIndex, rowIndex:rowState.rowIndex
+      });
+      setRowdataState({
+        ...rowdataState, data:rowData
+      });
+      
+      console.log(rowData, rowState);
+    },
   };
+  // validation functions
+  const required = value => (value == null ? 'Required' : undefined);
 
   const { classes } = props;
   // const [cancelState, setCancelState] = useState();
-
-
+  const [modalState, setModalState] = useState({
+    open: false,
+  })
+  const [progressState, setProgressState] = useState({
+    open: false,
+  })
+  const [completeState, setCompleteState] = useState({
+    open: false,
+  })
+  
+  const [rowindexState, setRowindexState] = useState({
+    rowIndex:0, dataIndex:0
+  })
+  const [rowdataState, setRowdataState]= useState({
+    ...rowdataState, data:[]
+  });
   const cancelOpen = () => {
-    
-    setCancelState({ ...cnacelState, open: true });
-    
+    setModalState({ ...modalState, open: true })
   }
-  const cnacelClose = () => {
-    setCancelState({ ...cancelState, open: false })
+  const cancelClose = () => {
+    setModalState({ ...modalState, open: false })
+  }
+  
+  const cancelClick = async() => {
+    setProgressState({...progressState, open:true});
+    setModalState({ ...modalState, open: false });  
+    // const formData= {
+    //   title: 'activity title',
+    //   sport: 1,
+    //   date: '2020-04-20',
+    //   time: '18:00',
+    //   duration: 60,
+    //   description: 'this is an activity description',
+    //   facilityId: '6807bae8-c51e-4343-bfef-31791a9f5488'
+    //   };
+    // const act_id = 'ef995215-5377-4e38-842a-0e2933bc54fb';
+    // const message =  'this is a message to all participants';
+    // await  cancelActivity(act_id, message);
+  
+    const facilityId = '3fc33045-21e6-494d-bc96-967ae26741b5'; 
+    const sorting = "date";
+    const re = await getActivities(facilityId, sorting);
+    console.log(re);
+    setCompleteState({ ...completeState, open: true });
+    setProgressState({...progressState, open:false});
+  };
+  
+  const completeClose = async() => {
+    setCompleteState({ ...completeState, open: false });
   }
   return (
     <div>
@@ -149,21 +210,76 @@ function AdvFilter(props) {
           {/* <DialogTitle id="alert-dialog-title">{'Add Supplier'}</DialogTitle> */}
           <DialogContent
             style={{
-              height: "200px", width: "400px"
+              height: "40rem", width: "36rem"
             }}
           >                  
             <Grid
               item
               xs={10}
               md={12}
-              className={classes.demo}    
-              style={{ paddingTop:"3rem" }}              
-            >
-              <FormLabel component="label" style={{ textAlign: "center"}}><h4 >{rowindexState.dataIndex}</h4></FormLabel>
-              <FormLabel component="label" style={{ textAlign: "center"}}><h4 ></h4></FormLabel>
-              {/* <FormLabel component="label" style={{ textAlign: "center"}}><h4 >row_data[2]</h4></FormLabel>
-              <FormLabel component="label" style={{ textAlign: "center"}}><h4 >row_data[3]</h4></FormLabel> */}
+              className={classes.demo}              
+            >       
+              <Typography variant="h4" component="h3">
+                Football Match
+              </Typography>
+              <Typography component="p">
+                2021/04/30 19:00~20:00
+              </Typography>
+              <hr/>
+              
+              <FormLabel component="label"><h4 >Number Of Participants:8</h4></FormLabel>
+              <br/>
+              
+              <Typography variant="h5" component="h5">
+                Message til Participants
+              </Typography>
         
+              <FormLabel component="label" style={{ paddingTop:'1rem' }}><h5>Messages will be sent to all participants who signed up for activity</h5></FormLabel>
+              <Input
+              placeholder="Type something"
+              // className={classes.input}
+              // inputProps={{
+              //   'aria-label': 'Description',
+              // }}
+              style={{ width:'100%' }}
+              // value={state.name} 
+              // onChange={onChangeName}
+              type = "text"
+              multiline={true}
+              rows={4}    
+            />
+              
+              {/* <Field
+                name="textarea"
+                className={classes.field}
+                component={TextFieldRedux}
+                placeholder="Type something"
+                label="Description"
+                multiline={true}
+                rows={4}
+              /> */}
+              
+              <FormLabel component="label"  style={{marginTop:'2rem' }}><h5>Confirm</h5></FormLabel>
+              {/* <Field
+                name="text"
+                component={TextFieldRedux}
+                placeholder="Name of Activity"
+                label="Text Field"
+                validate={required}
+                required
+                className={classes.field}
+              /> */}
+              <Input
+              placeholder='Write "confirm" to cancel activity'
+              // className={classes.input}
+              // inputProps={{
+              //   'aria-label': 'Description',
+              // }}
+              style={{ width:'100%' }}
+              // value={state.name} 
+              // onChange={onChangeName}
+              type = "text" 
+            />
             </Grid>
           </DialogContent>
           <DialogActions>
@@ -244,7 +360,7 @@ function AdvFilter(props) {
         </Dialog>
       </div>  
       </Grid>
-    </div>
+  </div>
   );
 }
 
